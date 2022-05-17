dc1ad447dbec71f599eeaef1c9e055d9d6aa2e34
Aldeer <gomilaemanuel@gmail.com>
2022-05-16 21:40:41 -0300

    creando mi primer componente y mostrandolo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import Contador from "./components/Contador";
 
 function App() {
   return (
     <div>
       <h1>Hola Mundo</h1>
+      <Contador />
     </div>
   );
 }
---
+++ src/components/Contador.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const contador = () => {
+    return (
+        <h3>Mi primer componente</h3>
+    );
+}
+
+export default contador;
